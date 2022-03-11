37c9f44857ee7d696971c2491d5f48c37aad1aa8
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2022-03-11 00:27:34 -0600

    Initial commit

---
+++ src/bingogameapp/BingoGameApp.java
@@ -0,0 +1,17 @@
+
+package bingogameapp;
+
+/**
+ *
+ * @author Daniel-Reyes
+ */
+public class BingoGameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
