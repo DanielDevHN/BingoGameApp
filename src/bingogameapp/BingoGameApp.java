45e30b0912b3dda8e2df7e6b4923f52ec84cb620
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2022-03-14 20:18:10 -0600

    Agregando class

--- src/bingogameapp/BingoGameApp.java
+++ src/bingogameapp/BingoGameApp.java
@@ -1,7 +1,6 @@
 
 package bingogameapp;
 
-import java.util.ArrayList;
 
 /**
  *
@@ -13,11 +12,7 @@ public class BingoGameApp {
      * @param args the command line arguments
      */
     
-    ArrayList lstB = new ArrayList();
-    ArrayList lstI = new ArrayList();
-    ArrayList lstN = new ArrayList();
-    ArrayList lstG = new ArrayList();
-    ArrayList lstO = new ArrayList();
+    
     
     public static void main(String[] args) {
         
